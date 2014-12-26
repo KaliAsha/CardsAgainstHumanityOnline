f7d1f58a637ed25253659561634c8a9ab636a4e0
Richie <me@richie.fr>
2014-12-26 04:21:13 +0100

    Cleanup & début intégration angular

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,5 +1,25 @@
 var app = angular.module('CAHApp', []);
 
-app.controller('CAHIndex', ['$scope', function($scope){
-	
+app.controller('CAHIndex', ['$scope', '$interval', function($scope, $interval){
+	console.log('CAHIndex init');
+
+	$scope.currPage = 'index';
+	$scope.loading  = true;
+
+
+	var socket = io.connect(server_url);
+	socket.on('connect', function(){
+		console.log('Connected.');
+		$scope.loading  = false;
+		$scope.$apply();
+	});
+	socket.on('error', function(err){
+		console.error('socket error', err);
+	});
+	socket.on('custom_event', function(data){
+		console.log('custom_event', data);
+	});
+
+	console.log(server_url);
+
 }])
